082c6d477c3bd74678192b6f340242d70ac8f4e9
太田 圭亮 <keisuke-ota@united.jp>
2018-06-05 09:58:27 +0900

    全ての壁との衝突判定追加

--- src/scene/game-scene.js
+++ src/scene/game-scene.js
@@ -7,7 +7,7 @@ scene.GameScene = (function() {
       this._super(param);
       cc.log(param);
 
-      this._time = 10;
+      this._time = 30;
       this._count = 0;
       this._playingFlg = false;
       this._views = {
@@ -20,7 +20,7 @@ scene.GameScene = (function() {
         btnLeft : null,
         buttonBack : null,
         player : null,
-        wall : null,
+        walls : [],
         goal : null
       };
     },
@@ -116,46 +116,60 @@ scene.GameScene = (function() {
         this.unscheduleUpdate();
       }
     },
+
+    _checkTouchingWithEachWall : function(offsetX, offsetY) {
+      //いずれかの壁とプレイヤーの接触判定
+      var playerRect = this._views.player.getBoundingBox();
+      //移動後のプレイヤーの場所で判定を行う（めり込んで動けなくなるの防止）
+      var movedPlayerRect = cc.rect({x: (playerRect.x + offsetX), y: (playerRect.y + offsetY), width: (playerRect.width), height: (playerRect.height)});
+
+      for(var i = 0; i < this._views.walls.length; i++){
+        var wall = this._views.walls[i];
+        var wallRect = wall.getBoundingBox();
+      
+        if(cc.rectIntersectsRect(movedPlayerRect, wallRect)){
+          return true;
+        }
+      }
+      return false;
+    },
+
     //仮の移動ボタン
     //壁があったら進めないように
     _onClickBtnRight : function(index) {
-      var playerRect = this._views.player.getBoundingBox();
-      var wallRect = this._views.wall.getBoundingBox();
-      var movedPlayerRect = cc.rect({x: (playerRect.x + 10), y: (playerRect.y), width: (playerRect.width), height: (playerRect.height)});
-      
-      if(!cc.rectIntersectsRect(movedPlayerRect, wallRect) && this._playingFlg){
-        var newPlayerPos = this._views.player.getPositionX() + 10;
-        this._views.player.setPositionX(newPlayerPos);
+      var offsetX = 10;
+      var offsetY = 0;
+      if(!this._checkTouchingWithEachWall(offsetX, offsetY) && this._playingFlg){
+        var newPlayerPosX = this._views.player.getPositionX() + offsetX;
+        var newPlayerPosY = this._views.player.getPositionY() + offsetY;
+        this._views.player.setPosition(newPlayerPosX, newPlayerPosY);
       }
     },
     _onClickBtnLeft : function(index) {
-      var playerRect = this._views.player.getBoundingBox();
-      var wallRect = this._views.wall.getBoundingBox();
-      var movedPlayerRect = cc.rect({x: (playerRect.x - 10), y: (playerRect.y), width: (playerRect.width), height: (playerRect.height)});
-      
-      if(!cc.rectIntersectsRect(movedPlayerRect, wallRect) && this._playingFlg){
-        var newPlayerPos = this._views.player.getPositionX() - 10;
-        this._views.player.setPositionX(newPlayerPos);
+      var offsetX = -10;
+      var offsetY = 0;
+      if(!this._checkTouchingWithEachWall(offsetX, offsetY) && this._playingFlg){
+        var newPlayerPosX = this._views.player.getPositionX() + offsetX;
+        var newPlayerPosY = this._views.player.getPositionY() + offsetY;
+        this._views.player.setPosition(newPlayerPosX, newPlayerPosY);
       }
     },
     _onClickBtnUp : function(index) {
-      var playerRect = this._views.player.getBoundingBox();
-      var wallRect = this._views.wall.getBoundingBox();
-      var movedPlayerRect = cc.rect({x: (playerRect.x), y: (playerRect.y + 10), width: (playerRect.width), height: (playerRect.height)});
-      
-      if(!cc.rectIntersectsRect(movedPlayerRect, wallRect) && this._playingFlg){
-        var newPlayerPos = this._views.player.getPositionY() + 10;
-        this._views.player.setPositionY(newPlayerPos);
+      var offsetX = 0;
+      var offsetY = 10;
+      if(!this._checkTouchingWithEachWall(offsetX, offsetY) && this._playingFlg){
+        var newPlayerPosX = this._views.player.getPositionX() + offsetX;
+        var newPlayerPosY = this._views.player.getPositionY() + offsetY;
+        this._views.player.setPosition(newPlayerPosX, newPlayerPosY);
       }
     },
     _onClickBtnDown : function(index) {
-      var playerRect = this._views.player.getBoundingBox();
-      var wallRect = this._views.wall.getBoundingBox();
-      var movedPlayerRect = cc.rect({x: (playerRect.x), y: (playerRect.y - 10), width: (playerRect.width), height: (playerRect.height)});
-      
-      if(!cc.rectIntersectsRect(movedPlayerRect, wallRect) && this._playingFlg){
-        var newPlayerPos = this._views.player.getPositionY() - 10;
-        this._views.player.setPositionY(newPlayerPos);
+      var offsetX = 0;
+      var offsetY = -10;
+      if(!this._checkTouchingWithEachWall(offsetX, offsetY) && this._playingFlg){
+        var newPlayerPosX = this._views.player.getPositionX() + offsetX;
+        var newPlayerPosY = this._views.player.getPositionY() + offsetY;
+        this._views.player.setPosition(newPlayerPosX, newPlayerPosY);
       }
     },
     _onClickButtonBack : function() {
